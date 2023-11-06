b337b904f6f8adb420f87f5d4a0fd3f57dc446d1
thaisdev <thaishdzlpz@gmail.com>
2023-11-06 22:01:23 +0000

    stage 5 passed

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 
-# Use curl to send custom HTTP requests and debug locally. Eg: curl -i -X GET http://localhost:4221/index.html
-
 import socket
 
 
@@ -18,18 +16,23 @@ def main():
     # receive data from the client
     data = connection_client.recv(1024)
     print(f"Received data {data.decode()}")
-    url = data.decode()
-    path = url.split(" ")[1]
-    if path.startswith('/echo/'):
-        response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
-        connection_client.send(response.encode()) # send a response back to the client
-    elif path == '/':
-        connection_client.send(b"HTTP/1.1 200 OK\r\n\r\n") 
+    url = data.decode().split("\r\n")
+    path = url[0].split(" ")[1]
+
+    if path == '/':
         #
         # esta wea se manda en bytes porque 
         # usa sockets en este caso con conexion TCP 
         # para la comunicacion
         # 
+        connection_client.send(b"HTTP/1.1 200 OK\r\n\r\n") 
+    elif path.startswith('/echo/'): 
+        response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
+        connection_client.send(response.encode()) # send a response back to the client
+    elif path == '/user-agent':
+        user_agent = url[2].split(" ")[1]
+        response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+        connection_client.send(response.encode())
     else:
         connection_client.send(b"HTTP/1.1 404 NOT FOUND\r\n\r\n") 
 
