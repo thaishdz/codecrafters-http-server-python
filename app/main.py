c8ef6fe64d8c07abde4327259bfd66e56c1dbdf3
thaisdev <thaishdzlpz@gmail.com>
2023-11-08 17:02:38 +0000

    stage 6 passed

--- app/main.py
+++ app/main.py
@@ -1,44 +1,46 @@
-
 import socket
+import threading
 
-
-def main():
-    
-    HOST, PORT = "localhost", 4221
-
-    server_socket = socket.create_server((HOST,PORT))
-    connection_client, addr = server_socket.accept()
-
+def handle_client(client_socket, addr):
     print(f"Got a connection from {addr}")
 
-    # Handle the connection - Here you can send/receive data, etc.
-    #
-    # receive data from the client
-    data = connection_client.recv(1024)
+    # Receive data from the client
+    data = client_socket.recv(1024)
     print(f"Received data {data.decode()}")
     url = data.decode().split("\r\n")
     path = url[0].split(" ")[1]
 
     if path == '/':
-        #
-        # esta wea se manda en bytes porque 
-        # usa sockets en este caso con conexion TCP 
-        # para la comunicacion
-        # 
-        connection_client.send(b"HTTP/1.1 200 OK\r\n\r\n") 
+        response = b"HTTP/1.1 200 OK\r\n\r\n"
     elif path.startswith('/echo/'): 
-        response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}"
-        connection_client.send(response.encode()) # send a response back to the client
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
     elif path == '/user-agent':
         user_agent = url[2].split(" ")[1]
-        response = f"HTTP/1.1 200 OK \r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-        connection_client.send(response.encode())
+        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
     else:
-        connection_client.send(b"HTTP/1.1 404 NOT FOUND\r\n\r\n") 
+        response = b"HTTP/1.1 404 NOT FOUND\r\n\r\n"
 
+    # Send the HTTP response back to the client
+    client_socket.send(response)
 
-    # close the client socket
-    connection_client.close()
+    # Close the client socket
+    client_socket.close()
+
+def main():
+    HOST, PORT = "localhost", 4221
+
+    server_socket = socket.create_server((HOST, PORT), reuse_port=True)
+    server_socket.listen(5)  # Listen for incoming connections
+
+    print(f"Server listening on {HOST}:{PORT}")
+
+    while True:
+        # Accept incoming connections
+        client_socket, addr = server_socket.accept()
+
+        # Create a new thread to handle the client
+        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
+        client_thread.start()
 
 if __name__ == "__main__":
     main()
