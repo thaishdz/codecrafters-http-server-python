41c0631ed76c27746dcbcfd4f1eda462a4be9e68
thaisdev <thaishdzlpz@gmail.com>
2023-11-16 22:44:00 +0000

    stage 7 OUT

--- app/main.py
+++ app/main.py
@@ -1,5 +1,41 @@
+import os
 import socket
 import threading
+import argparse
+
+def parse_argument():
+   parser = argparse.ArgumentParser()
+   parser.add_argument("--directory", required=True)
+   args = parser.parse_args()
+   return args.directory
+
+def handle_response(content_type = None, data = None, status_code = 200):
+
+    if status_code == 200:
+        HEADER_HTTP = 'HTTP/1.1 200 OK'
+        CRLF = '\r\n'
+        CONTENT_TYPE = 'Content-Type: '
+        CONTENT_LENGTH = 'Content-Length: '
+        print(data)
+        return f"{HEADER_HTTP}{CRLF}{CONTENT_TYPE}{content_type}{CRLF}{CONTENT_LENGTH}{len(data)}{CRLF}{CRLF}{data}".encode()
+    else:
+        HEADER_HTTP = 'HTTP/1.1 404 NOT FOUND'
+        CRLF = '\r\n'
+        CONTENT_TYPE = 'Content-Type: '
+        CONTENT_LENGTH = 'Content-Length: '
+
+        return f"{HEADER_HTTP}{CRLF}{CRLF}".encode()
+        
+def handle_file_request(directory,filename):
+    # this line not working for me :(, I dunno why -> os.path.join(serving_directory, filename)
+    filename = filename[1:]
+    file_path = f"{directory}{filename}"  # Build the full file path
+    if os.path.isfile(file_path):  # Check if the file exists
+        with open(file_path, 'rb') as f:
+            file_content = f.read().decode()
+        return handle_response('application/octet-stream',file_content)
+    else:
+        return handle_response(status_code=404)
 
 def handle_client(client_socket, addr):
     print(f"Got a connection from {addr}")
@@ -13,12 +49,16 @@ def handle_client(client_socket, addr):
     if path == '/':
         response = b"HTTP/1.1 200 OK\r\n\r\n"
     elif path.startswith('/echo/'): 
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(path[6:])}\r\n\r\n{path[6:]}".encode()
+        response = handle_response('text/plain',path[6:])
     elif path == '/user-agent':
         user_agent = url[2].split(" ")[1]
-        response = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}".encode()
+        response = handle_response('text/plain',user_agent)
+    elif path.startswith('/files'):
+        filename = path.removeprefix('/files')
+        directory = parse_argument()
+        response = handle_file_request(directory,filename)
     else:
-        response = b"HTTP/1.1 404 NOT FOUND\r\n\r\n"
+        response = handle_response(status_code=404)
 
     # Send the HTTP response back to the client
     client_socket.send(response)
